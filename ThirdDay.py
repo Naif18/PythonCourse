3d456f7cdce26c9291a5861fb65885baa1d473d5
Naif <naif.naasr@gmail.com>
2019-08-23 14:34:21 +0300

    ThirdDay

---
+++ ThirdDay.py
@@ -0,0 +1,31 @@
+x = "Naif" 
+print(x) 
+x = 4 
+print(x)
+
+y = 'Naif'
+print(y)
+y = "Naif"
+print(y)
+
+#-------------------------------------------------------
+x , y , s = "XVariable" , "YVariable" ,"SVariable"
+
+print(x , y ,s)
+
+x = y = s = "MultipleVariables"
+print(x , y ,s)
+
+#-------------------------------------------
+x= "Awesome"
+print("Python is " + x)
+
+x= "Pyhon is"
+y= " Awesome"
+z = x + y
+print(z)
+
+#---------------------
+x = 5
+y = 10
+print(x + y)
