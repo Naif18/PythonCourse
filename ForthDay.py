479ca41e62df3e089bde13ced8f842f0e5f6427d
Naif <naif.naasr@gmail.com>
2019-08-23 20:05:52 +0300

    ForthDay

---
+++ ForthDay.py
@@ -0,0 +1,47 @@
+x = 12
+y = -306
+z = 3420040000
+print(type(x))
+print(type(y))
+print(type(z))
+
+#-------------
+
+x = -12.5
+y = 1.0
+print(type(x) , type(y))
+
+#-------------------------
+
+x = 35e5
+y = 50E2
+print(type(x))
+print(type(y))
+
+#------------
+
+x = 3 + 5j
+y = 5j
+z= -5j
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+#-------------
+
+x = 10
+y = 2.5
+z = 5j
+
+a= int(y)
+b= float(x)
+c= complex(x)
+
+print(type(a))
+print(type(b))
+print(type(c))
+
+#---------------
+import random
+print (random.randrange(10,60))
