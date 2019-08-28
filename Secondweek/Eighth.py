69c7dfe5b417b935ecea3f9579920c884f7e62f5
Naif <naif.naasr@gmail.com>
2019-08-28 22:11:29 +0300

    EighthDay

---
+++ Secondweek/Eighth.py
@@ -0,0 +1,12 @@
+a= " Hello,world "
+print(a)
+#lengh of string
+print(len(a))
+#convert to lowercase
+print(a.lower())
+#convert to Uppercase
+print(a.upper())
+#
+print(a.replace("H" , "F" ))
+
+print(a.split(","))
