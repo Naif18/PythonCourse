ed50ee0265e24cb2a9b710cad6796b2fc7010c3b
Naif <naif.naasr@gmail.com>
2019-09-01 13:52:22 +0300

    TenthDay

---
+++ Secondweek/Ninth.py
@@ -0,0 +1,4 @@
+age = 36
+txt = "My name is naif, and I am {}"
+print(txt.format(age))
+    
---
+++ Secondweek/Tenth.py
@@ -0,0 +1,13 @@
+x = 10 
+y = 5
+print(x/y)
+
+x = 3
+z=x**x
+print(z)
+
+x^=5
+print(x)
+
+x>= 3
+print(x)
