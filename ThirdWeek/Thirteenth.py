47dfb511f4deb0c9ed23632bb505764d8085515f
Naif <naif.naasr@gmail.com>
2019-09-02 09:16:00 +0300

    ThirdDay

---
+++ ThirdWeek/Thirteenth.py
@@ -0,0 +1,13 @@
+lists=[1,2,44,"list", 5.8]
+print(lists)
+
+Decimal_numbers = [5.5 , 2.0 , 99.5 , 8.4]
+print(Decimal_numbers)
+print(Decimal_numbers[3])
+
+for x in Decimal_numbers:
+    print(x)
+
+Decimal_numbers[2] = 5
+del Decimal_numbers[0]
+print(x)
