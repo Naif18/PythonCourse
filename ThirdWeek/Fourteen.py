41eeb7dd72988b503ac8e4ad9d4d37cb741236fc
Naif <naif.naasr@gmail.com>
2019-09-04 09:38:29 +0300

    FourteenDay

---
+++ ThirdWeek/Fourteen.py
@@ -0,0 +1,14 @@
+Lists=[2,3,5,66,75,11,90]
+print(Lists[2:4])
+
+if 66 in Lists:
+    print("Yes, the Number is in the list")
+
+
+#------------------------------------
+RepeatList=["PythonLanguge"]*3
+print(RepeatList)
+
+stringList=["Ahmed","Khalid","Saad"]
+combine=Lists+stringList
+print(combine)
