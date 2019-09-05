bb4b6a0b558b1b97964a86e1a5cf9031baa056b4
Naif <naif.naasr@gmail.com>
2019-09-05 11:48:06 +0300

    Sixteenth

---
+++ ThirdWeek/Sixteenth.py
@@ -0,0 +1,16 @@
+tuplee=(3)
+print(tuplee)
+
+complextuple=(5,4,3,77,"Python")    
+print(complextuple)
+print(complextuple[4])
+
+#You cann't change tuple items. it's unchangeble
+#You cann't also inser or delete items (:
+
+for t in  complextuple:
+    print(t)
+
+del tuplee
+
+print (complextuple[2:5])
