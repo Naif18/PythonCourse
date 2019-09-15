70002dde321350fa6abdf1110b1b3dd0f08699c8
Naif <naif.naasr@gmail.com>
2019-09-15 12:22:01 +0300

    Challenge toenty-fifth and twenty-sixth

---
+++ ForthWeek/TwentyfiveandTwentySix.py
@@ -0,0 +1,21 @@
+#Challenge today twenty-fifth and twenty-sixth
+
+#Challenge Number 1
+Myset = { 1 , 3, 5 , 7 }
+Myset.update([4 , 8 , 1 , 2] )
+Myset.remove(8)
+print(Myset)
+
+#Challenge Number 2
+
+dic= {
+    "Name" : "pigeon",
+    "type" : "bird" ,
+     "skin over" : "feathers"
+}
+x = dic.get("type")
+print(x)
+
+dic["skin over"]  = "new skin"
+
+print(dic)
