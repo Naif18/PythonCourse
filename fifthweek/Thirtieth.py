d613318275b265d69954b6703800acf4c1846a9e
Naif <naif.naasr@gmail.com>
2019-09-26 10:14:56 +0300

    Thirtyeth

---
+++ fifthweek/Thirtieth.py
@@ -0,0 +1,26 @@
+#ٌrange Function: set of code a specified number of time
+
+for x in range(8):
+    print(x)
+
+for z in range(4 , 11):
+    print(z)
+
+#Also you can sspecify the number of incrases 
+
+for z in range(4 , 60 , 10):
+    print(z)
+else:
+    print("Finally finished!")
+
+
+#nested loops. ...
+col=["Red" , "White" , "Green" , "Yellow"]
+adj=["good" , "Very Good" , "Nice" , "beaty"]
+
+for x in col:
+    for z in adj:
+        print(x,z)
+        
+
+
