00d5e35e5124bb6ac9d6cc0349f69b055f1c0b71
Naif <naif.naasr@gmail.com>
2019-09-26 11:57:50 +0300

    ThirtyOne Day

---
+++ fifthweek/Thirtyone.py
@@ -0,0 +1,19 @@
+#PYTHON FUNCTIONS:
+
+def fun():
+    print("Hellow Function")
+fun() 
+#--------------
+
+def fun2(fname):
+    print(fname , "Hello")
+
+fun2("Naif")
+
+#Default parameter value
+
+def fun3(countery ="Saudi"):
+    print("I am from " + countery)
+
+fun3()
+fun3("USA")
